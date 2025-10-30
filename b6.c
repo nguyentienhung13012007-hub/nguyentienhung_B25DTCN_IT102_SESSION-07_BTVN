1aaadc12e812f39743b31a6e7c4fd3d43e6eb53c
nguyentienhung13012007-hub <nguyentienhung13012007@gmail.com>
2025-10-30 21:27:40 +0700

    Update b6.c

--- b6.c
+++ b6.c
@@ -12,10 +12,12 @@ int main() {
     while (n != 0) {
         unit = n % 10;                
         daoNguoc = daoNguoc * 10 + unit; 
+        n=n/10;
     }
 
     printf("So dao nguoc cua %d la: %d\n", temp, daoNguoc);
 
     return 0;
 }
+
 
